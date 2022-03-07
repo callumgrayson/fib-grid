2588e679f0b217654cdd2d16f7e763e7237834ef
Callum <ztlimp@gmail.com>
2022-03-07 08:29:14 +0100

    Enables clearing of sequences starting with numbers other than 1

--- src/CONSTANTS.js
+++ src/CONSTANTS.js
@@ -4,8 +4,16 @@ export const classNames_Cell = {
   default: "color-default",
 };
 
+// First 25 terms - the 25th term would take over a day to create by clicking once a second
+// Will suffice in practical terms
+export const fibTerms = [
+  1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584,
+  4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393,
+];
+
 const CONSTANTS = {
   classNames_Cell,
+  fibTerms,
 };
 
 export default CONSTANTS;
--- src/helpers/checkCellsToClear.js
+++ src/helpers/checkCellsToClear.js
@@ -1,4 +1,4 @@
-const fibTerms = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]; // Terms less than 100
+import { fibTerms } from "../CONSTANTS";
 const SEQUENCE_LENGTH = 5;
 
 const directions = [
@@ -24,48 +24,60 @@ function checkCellsToClear(grid) {
     for (let c = 0; c < row.length; c++) {
       const cell = row[c];
 
-      // If the cell count is 1 start checks to see if any sequences are present
+      // If the cell count is any number in the first 20 fibTerms start checks to see if any sequences are present
+      // This is an array of indices since there are two 1s, which both need checking
+      let fibTermIndices = [fibTerms.slice(0, 20).indexOf(cell.count)];
+
+      // Since there are two 1s in the list run checks on two starting points, ie:
+      // 1, 1, 2, 3,...
+      // 1, 2, 3, 5,...
       if (cell.count === 1) {
-        // Push cell into list
-        let checkSequence = [[c, r]];
+        fibTermIndices = [0, 1];
+      }
 
-        // Check each direction
-        for (let dir = 0; dir < directions.length; dir++) {
-          const direction = directions[dir];
+      for (let fibIndex = 0; fibIndex < fibTermIndices.length; fibIndex++) {
+        if (fibTermIndices[fibIndex] >= 0) {
+          // Push cell into list
+          let checkSequence = [[c, r]];
 
-          // Reset list before checking this direction
-          checkSequence = [[c, r]];
+          // Check each direction
+          for (let dir = 0; dir < directions.length; dir++) {
+            const direction = directions[dir];
 
-          // check each cell along vector
-          for (
-            let vectorIndex = 1;
-            vectorIndex < SEQUENCE_LENGTH;
-            vectorIndex++
-          ) {
-            // const vectorCell = array[vectorIndex];
-            const lastCell = checkSequence[checkSequence.length - 1];
-            const nextCell = [
-              lastCell[0] + direction[0],
-              lastCell[1] + direction[1],
-            ];
+            // Reset list before checking this direction
+            checkSequence = [[c, r]];
 
-            // Check that no cells are out of bounds
-            if (
-              nextCell[0] < 0 ||
-              nextCell[1] < 0 ||
-              nextCell[0] >= grid.length ||
-              nextCell[1] >= grid.length
-            )
-              break;
+            // check each cell along vector
+            for (
+              let vectorIndex = 1;
+              vectorIndex < SEQUENCE_LENGTH;
+              vectorIndex++
+            ) {
+              // const vectorCell = array[vectorIndex];
+              const lastCell = checkSequence[checkSequence.length - 1];
+              const nextCell = [
+                lastCell[0] + direction[0],
+                lastCell[1] + direction[1],
+              ];
 
-            const cellObject = grid[nextCell[1]][nextCell[0]];
-            const count = cellObject && cellObject?.count;
-            const term = fibTerms[vectorIndex];
-            if (count === term) {
-              checkSequence.push(nextCell);
-            }
-            if (checkSequence.length === SEQUENCE_LENGTH) {
-              sequences.push(...checkSequence);
+              // Check that no cells are out of bounds
+              if (
+                nextCell[0] < 0 ||
+                nextCell[1] < 0 ||
+                nextCell[0] >= grid.length ||
+                nextCell[1] >= grid.length
+              )
+                break;
+
+              const cellObject = grid[nextCell[1]][nextCell[0]];
+              const count = cellObject && cellObject?.count;
+              const term = fibTerms[vectorIndex + fibTermIndices[fibIndex]];
+              if (count === term) {
+                checkSequence.push(nextCell);
+              }
+              if (checkSequence.length === SEQUENCE_LENGTH) {
+                sequences.push(...checkSequence);
+              }
             }
           }
         }
